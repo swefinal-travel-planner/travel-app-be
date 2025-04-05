5a3a8ae5ccb4e2d296f73ff2a8e2fb1c0e510805
Tricker14 <trietthetricker@gmail.com>
2025-04-06 02:07:53 +0700

    fix: update mail client

--- internal/bean/mail_client.go
+++ internal/bean/mail_client.go
@@ -7,6 +7,5 @@ import (
 
 type MailClient interface {
 	GenerateOTPBody(to, code, context string, ttl time.Duration) string
-	GenerateRandomPasswordBody(to, password string) string
 	SendEmail(ctx context.Context, to, subject, body string) error
 }
