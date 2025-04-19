5a2e2207931643085179f92e7173686e97e907ad
VuKhoa23 <vuanhkhoa007@gmail.com>
2025-04-20 00:21:49 +0700

    hotfix: dockerfile

--- main.go
+++ main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"os"
 
+	_ "github.com/swefinal-travel-planner/travel-app-be/docs"
+
 	"github.com/swefinal-travel-planner/travel-app-be/startup"
 )
 
+// @title Travel App API
+// @version 1.0
+// @description This is a travel planning application API
 // @BasePath /api/v1
 func main() {
 	if len(os.Args) > 1 && os.Args[1] == "migrate-up" {
