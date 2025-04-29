dd244d958eea50cee44ed05a061e4498dbaf718a
VuKhoa23 <vuanhkhoa007@gmail.com>
2025-04-29 17:04:07 +0700

    feat: init uow

---
+++ internal/repository/implement/unit_of_work.go
@@ -0,0 +1,35 @@
+package repositoryimplement
+
+import (
+    "context"
+    "database/sql"
+
+    "github.com/jmoiron/sqlx"
+    "github.com/swefinal-travel-planner/travel-app-be/internal/repository"
+)
+
+type UnitOfWorkImpl struct {
+    db *sqlx.DB
+}
+
+func NewUnitOfWork(db *sqlx.DB) repository.UnitOfWork {
+    return &UnitOfWorkImpl{db: db}
+}
+
+func (uow *UnitOfWorkImpl) Begin(ctx context.Context) (*sqlx.Tx, error) {
+    return uow.db.BeginTxx(ctx, &sql.TxOptions{})
+}
+
+func (uow *UnitOfWorkImpl) Commit(tx *sqlx.Tx) error {
+    if tx != nil {
+        return tx.Commit()
+    }
+    return nil
+}
+
+func (uow *UnitOfWorkImpl) Rollback(tx *sqlx.Tx) error {
+    if tx != nil {
+        return tx.Rollback()
+    }
+    return nil
+}
