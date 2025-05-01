1deb5bfa20f8e1555bf31c4a51d6a5649b84bf0a
Tricker14 <trietthetricker@gmail.com>
2025-05-01 22:13:03 +0700

    feat: add migration for trip, trip_member, trip_item

---
+++ migrations/20250501221223_create_trip_items_table.up.sql
@@ -0,0 +1,12 @@
+CREATE TABLE trip_items (
+   id INT AUTO_INCREMENT PRIMARY KEY,
+   trip_id INT NOT NULL,
+   place_id VARCHAR(255) UNIQUE NOT NULL,
+   trip_day INT NOT NULL,
+   order_in_day INT NOT NULL,
+   tag ENUM('food_location', 'travel_location') NOT NULL,
+   CONSTRAINT fk_trip_trip_item FOREIGN KEY (trip_id) REFERENCES trips(id) ON DELETE CASCADE,
+   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+   deleted_at TIMESTAMP NULL DEFAULT NULL
+);
