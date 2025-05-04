01d0daca55a5fb755c3a65be1039bfa9834bcd1a
VuKhoa23 <vuanhkhoa007@gmail.com>
2025-05-04 18:47:05 +0700

    feat: clean

--- internal/service/trip_service.go
+++ internal/service/trip_service.go
@@ -7,6 +7,4 @@ import (
 
 type TripService interface {
 	CreateTrip(ctx *gin.Context, tripRequest model.TripRequest, userId int64) (int64, string)
-	GetTrip(ctx *gin.Context, tripId int64) (*model.TripResponse, string)
-	GetTripsByUserId(ctx *gin.Context, userId int64) ([]model.TripResponse, string)
 }
